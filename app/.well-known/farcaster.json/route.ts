a02aa1377ecc4cb33a7a74a5e61b57870994fc1e
Aryan Godara <aryangodara03@gmail.com>
2025-08-17 23:25:23 +0530

    fix constraints

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -5,10 +5,10 @@ export async function GET() {
   const farcasterConfig = {
     accountAssociation: {
       header:
-        "eyJmaWQiOjUwMDkxNiwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDM2Y2E5RjMwRDQ4YWNENUUxRkExMGVEOTk1NzgzRWREODc0MWViM0YifQ",
-      payload: "eyJkb21haW4iOiJidXp6YmFzZS5mdW4ifQ",
+        "",
+      payload: "",
       signature:
-        "Fts8ZVTjOnROoeA+j4KeOYQcgGg/h1W7NCKkMIrVAn1n3sHxJIaVgkJ34rOhFclDyY2EOYGifUurh1A5I0jO6Rw=",
+        "",
     },
     frame: {
       version: "1",
@@ -18,7 +18,7 @@ export async function GET() {
       imageUrl: `${APP_URL}/logo.png`,
       subtitle: "Are you buzzing enough?",
       description:
-        "Are you buzzing enough? Check your creator score, compare with friends, and unlock funding based on your social influence. Top creators get the best rates. See where you stand!",
+        "Are you buzzing? Check your creator score, compare with friends, and unlock funding based on your social influence. Top creators get the best rates. See where you stand",
       screenshotUrls: [
         `${APP_URL}/screenshots/ss1.png`,
         `${APP_URL}/screenshots/ss2.png`,
