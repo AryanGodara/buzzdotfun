067f3bd4133bbb8cf8651acb74a642c09e05babe
Aryan Godara <aryangodara03@gmail.com>
2025-08-17 20:20:29 +0530

    update route.ts with farcaster.json data

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,21 +3,33 @@ import { APP_URL } from '../../../lib/constants'
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add your own account association
     frame: {
+      name: 'Buzz.fun',
       version: '1',
-      name: 'Monad Farcaster MiniApp Template',
-      iconUrl: `${APP_URL}/images/icon.png`,
+      iconUrl: `${APP_URL}/icon.png`,
       homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ['monad', 'farcaster', 'miniapp', 'template'],
-      primaryCategory: 'developer-tools',
-      buttonTitle: 'Launch Template',
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: '#ffffff',
+      imageUrl: `${APP_URL}/logo.png`,
+      buttonTitle: 'Rate My Buzz',
+      splashImageUrl: `${APP_URL}/splash.png`,
+      splashBackgroundColor: '#FFFFFF',
       webhookUrl: `${APP_URL}/api/webhook`,
+      subtitle: 'Are you buzzing enough?',
+      description: 'Are you buzzing enough? Check your creator score, compare with friends, and unlock funding based on your social influence. Top creators get the best rates. See where you stand!',
+      primaryCategory: 'social',
+      tags: [
+        'social',
+        'community',
+        'creator',
+        'defi',
+        'finance'
+      ],
+      tagline: 'Create buzz. Build credit.'
     },
+    accountAssociation: {
+      header: 'eyJmaWQiOjUwMDkxNiwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDM2Y2E5RjMwRDQ4YWNENUUxRkExMGVEOTk1NzgzRWREODc0MWViM0YifQ',
+      payload: 'eyJkb21haW4iOiJidXp6YmFzZS5mdW4ifQ',
+      signature: 'Fts8ZVTjOnROoeA+j4KeOYQcgGg/h1W7NCKkMIrVAn1n3sHxJIaVgkJ34rOhFclDyY2EOYGifUurh1A5I0jO6Rw='
+    }
   }
 
   return NextResponse.json(farcasterConfig)
